95ccd5985e19e901b2ab2cb9069795e7bbd78d0e
sbrown-si <sbrown@shelterinsurance.com>
2024-06-17 13:07:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,6 @@ ingredients_list=st.multiselect(
     , max_selections=5
     )
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
-            
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
