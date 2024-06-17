eab9bdbd7e8539f5a255adf0b345fce1440b13cc
sbrown-si <sbrown@shelterinsurance.com>
2024-06-17 10:28:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered', icon="✅")
 
+##New section to display fruityvice nutrition information
+import reqests
+fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
