0cf7d583e5418a44cdffb5d37018c02d351047ac
sbrown-si <sbrown@shelterinsurance.com>
2024-06-17 11:10:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
